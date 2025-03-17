650d54c2251b4783e5ab337994d82fac6cffc4d5
Daniel Robledo <daniel.canorobledo@gmail.com>
2025-03-17 11:29:40 +0100

    First commit

---
+++ Backend/src/main/java/com/drobledo/fullstacktips/FullStackTipsApplication.java
@@ -0,0 +1,13 @@
+package com.drobledo.fullstacktips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackTipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackTipsApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/drobledo/fullstacktips/FullStackTipsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drobledo.fullstacktips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackTipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
