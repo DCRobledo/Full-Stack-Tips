b70afb2f1449c05952a8925d4931b97ba6ca56fb
Daniel Robledo <daniel.canorobledo@gmail.com>
2025-03-21 16:00:19 +0100

    Implemented Singleton pattern on frontend

---
+++ Frontend/src/Singleton/AppSingleton.tsx
@@ -0,0 +1,19 @@
+﻿import {useEffect, useState} from "react";
+import Config from "./Config.ts";
+
+const AppSingleton = () => {
+    const [APIURL, setAPIURL] = useState('');
+
+    useEffect(() => {
+        const config = Config.getInstance();
+        setAPIURL(config.APIURL);
+    }, []);
+
+    return (
+        <div>
+            API URL: {APIURL}
+        </div>
+    )
+}
+
+export default AppSingleton;
---
+++ Frontend/src/Singleton/Config.ts
@@ -0,0 +1,22 @@
+﻿class Config {
+    private static instance: Config;
+    public readonly APIURL: string;
+
+
+    private constructor() 
+    {
+        this.APIURL = "https://youshouldhiredanielrobledo.com"
+    }
+    
+    public static getInstance(): Config 
+    {
+        if (Config.instance == null)
+        {
+            Config.instance = new Config();
+        }
+        
+        return Config.instance;
+    }
+}
+
+export default Config;
